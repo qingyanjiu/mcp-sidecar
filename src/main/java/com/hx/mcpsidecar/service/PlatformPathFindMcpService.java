a8a89fdda9b6d3069b0a980b71cade12a244b738
LouisLiu <qingyanjiu@hotmail.com>
2026-02-11 09:19:41 +0800

    提取各接口URL请求地址为static字符串管理

--- src/main/java/com/hx/mcpsidecar/service/PlatformPathFindMcpService.java
+++ src/main/java/com/hx/mcpsidecar/service/PlatformPathFindMcpService.java
@@ -15,6 +15,9 @@ public class PlatformPathFindMcpService {
     private static final String TOOL_NAME_PREFIX = "path_find_service:";
     private static final String TOOL_DESC_PREFIX = "路径规划类服务:";
 
+    // 查询路径规划配置列表接口URL
+    public static final String PATH_FIND_GRID_LIST_URL = "/visualPathFindGrid/listOnly";
+
     @Autowired
     @Qualifier("platformApiCallServiceImpl")
     private ApiCallService platformApiCallServiceImpl;
@@ -26,7 +29,7 @@ public class PlatformPathFindMcpService {
             """
     )
     public String getPathFindGridList() {
-        String url = platformApiCallServiceImpl.getBaseUrl() + "/visualPathFindGrid/listOnly";
+        String url = platformApiCallServiceImpl.getBaseUrl() + PATH_FIND_GRID_LIST_URL;
         Object result = platformApiCallServiceImpl.doGetCall(url);
         return JSON.toJSONString(result);
     }
