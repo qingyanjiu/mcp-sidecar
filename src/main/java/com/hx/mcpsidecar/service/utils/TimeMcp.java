295d6bc95d80acd3918f1dbd11bf152a9d763b13
LouisLiu <qingyanjiu@hotmail.com>
2026-03-02 11:28:15 +0800

    增加新的mcp服务

---
+++ src/main/java/com/hx/mcpsidecar/service/utils/TimeMcp.java
@@ -0,0 +1,25 @@
+package com.hx.mcpsidecar.service.utils;
+
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springaicommunity.mcp.annotation.McpToolParam;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class TimeMcp {
+
+    private static final String TOOL_NAME_PREFIX = "time_service:";
+    private static final String TOOL_DESC_PREFIX = "时间类服务:";
+
+    @McpTool(
+        name = TOOL_NAME_PREFIX + "getCurrentDate",
+        description = TOOL_DESC_PREFIX + """
+            获取当前日期
+            """
+    )
+    public String getCurrentDate() {
+        return LocalDate.now().toString();
+    }
+
+}
